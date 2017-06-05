f48eda324e7be3afc60ef7c88535619c61f0e3cb
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-06-05 13:50:48 +0000

    twitter追加

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,15 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def twitter
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"])
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"].except('extra')
+      redirect_to new_user_registration_url
+    end
+  end
 
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,21 @@ class User < ApplicationRecord
         user.name = auth.info.name
       end
     end
+
+    def find_for_twitter_oauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = User.create_unique_email
+        user.password = Devise.friendly_token[0,20]
+        user.name = auth.info.name
+      end
+    end
+
+    def create_unique_string
+      SecureRandom.uuid
+    end
+
+    def create_unique_email
+      User.create_unique_string + "@example.com"
+    end
   end
 end
